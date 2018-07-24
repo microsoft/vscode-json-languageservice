2a389b410ebd0b684a1c008c458f3540f1d467be
Martin Aeschlimann <martinae@microsoft.com>
2018-07-24 10:02:27 +0200

    update vscode-languageserver-types: colors and folding ranges

--- src/jsonLanguageTypes.ts
+++ src/jsonLanguageTypes.ts
@@ -6,9 +6,10 @@
 
 import { JSONWorkerContribution, JSONPath, Segment, CompletionsCollector } from './jsonContributions';
 import { JSONSchema } from './jsonSchema';
-import { Range, TextEdit } from 'vscode-languageserver-types';
+import { Range, TextEdit, Color, ColorInformation, ColorPresentation, FoldingRange, FoldingRangeKind } from 'vscode-languageserver-types';
 
-export { Range, TextEdit, JSONSchema, JSONWorkerContribution, JSONPath, Segment, CompletionsCollector };
+export { Range, TextEdit, JSONSchema, JSONWorkerContribution, JSONPath, Segment, CompletionsCollector, 
+	Color, ColorInformation, ColorPresentation, FoldingRange, FoldingRangeKind };
 
 /**
  * Error codes used by diagnostics
@@ -77,86 +78,6 @@ export interface BooleanASTNode extends BaseASTNode {
 export interface NullASTNode extends BaseASTNode {
 	readonly type: 'null';
 	readonly value: null;
-}
-
-export interface Color {
-	red: number; blue: number; green: number; alpha: number;
-}
-
-export interface ColorInformation {
-	range: Range;
-	color: Color;
-}
-
-export interface ColorPresentation {
-	/**
-	 * The label of this color presentation. It will be shown on the color
-	 * picker header. By default this is also the text that is inserted when selecting
-	 * this color presentation.
-	 */
-	label: string;
-	/**
-	 * An [edit](#TextEdit) which is applied to a document when selecting
-	 * this presentation for the color.  When `falsy` the [label](#ColorPresentation.label)
-	 * is used.
-	 */
-	textEdit?: TextEdit;
-	/**
-	 * An optional array of additional [text edits](#TextEdit) that are applied when
-	 * selecting this color presentation. Edits must not overlap with the main [edit](#ColorPresentation.textEdit) nor with themselves.
-	 */
-	additionalTextEdits?: TextEdit[];
-}
-
-/**
- * Enum of known range kinds
- */
-export enum FoldingRangeKind {
-	/**
-	 * Folding range for a comment
-	 */
-	Comment = 'comment',
-	/**
-	 * Folding range for a imports or includes
-	 */
-	Imports = 'imports',
-	/**
-	 * Folding range for a region (e.g. `#region`)
-	 */
-	Region = 'region'
-}
-
-/**
- * Represents a folding range.
- */
-export interface FoldingRange {
-
-	/**
-	 * The zero-based line number from where the folded range starts.
-	 */
-	startLine: number;
-
-	/**
-	 * The zero-based character offset from where the folded range starts. If not defined, defaults to the length of the start line.
-	 */
-	startCharacter?: number;
-
-	/**
-	 * The zero-based line number where the folded range ends.
-	 */
-	endLine: number;
-
-	/**
-	 * The zero-based character offset before the folded range ends. If not defined, defaults to the length of the end line.
-	 */
-	endCharacter?: number;
-
-	/**
-	 * Describes the kind of the folding range such as `comment' or 'region'. The kind
-	 * is used to categorize folding ranges and used by commands like 'Fold all comments'. See
-	 * [FoldingRangeKind](#FoldingRangeKind) for an enumeration of standardized kinds.
-	 */
-	kind?: string;
 }
 
 export interface LanguageSettings {
